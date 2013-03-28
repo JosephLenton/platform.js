72c33d95583de1e6548b77cc5b2d6872095a5bc5
Joseph Lenton <jxl299@cs.bham.ac.uk>
2013-03-28 11:45:04 +0000

    abc.jsx re-added, and changed to avoid object properties
    abc.jsx debugging library has been added back,
    and changed so that the properties are no longer
    visibile to object during iteration.

---
+++ src/libs/abc.jsx
@@ -0,0 +1,565 @@
+
+abc.js
+======
+
+@author Joseph Lenton
+
+A debugging library based on Letters
+for Ruby, http://lettersrb.com
+
+Adds methods to the Object prototype,
+for quick debugging at runtime.
+
+ * a is for assertion
+ * b is for block
+ * c is for call method
+ * d is for debug
+ * e if for asserts if this is empty
+ * f is for print field
+
+ * k is for printing keys
+
+ * l is for log console
+
+ * m is for mark object
+ * n is for no mark (unmark object)
+
+ * p is for print
+
+ * s is for stack trace
+ * t is for timestamp
+ * u is for user alert
+
+ * v is for printing values
+
+### Marking
+
+Some functions allow you to mark / unmark,
+or filter based on mark.
+
+By 'mark' it means setting an identifier to
+that object. Why? Sometimes in large systems,
+you have lots of objects floating around,
+and being pushed through single functions.
+
+Marking is a way for you to mark objects in
+the data set before a function call, and then
+easily see if they turn up later in other parts
+of your program.
+
+'true' represents 'all marks', and is used
+
+when you ask to mark, but don't specify it.
+
+
+-------------------------------------------------------------------------------
+
+### shim
+
+Used to set the properties to the object. This tries to make the properties not
+show up when enumerated, but this only works in IE 8 and above.
+
+-------------------------------------------------------------------------------
+
+    var shim = function( obj, props ) {
+        for ( var k in props ) {
+            if ( props.hasOwnProperty(k) && !obj.hasOwnProperty(k) ) {
+                if ( Object.defineProperty !== undefined ) {
+                    Object.defineProperty( obj, k, {
+                            value           : props[k], 
+                            enumerable      : false,
+                            writable        : true,
+                            configurable    : true
+                    } );
+                } else {
+                    obj[k] = props[k];
+                }
+            }
+        }
+    }
+    
+
+
+    shim( Object.prototype, {
+
+-------------------------------------------------------------------------------
+
+## Assertion
+
+@example
+
+    foo.a()
+        // asserts 'this'
+
+@example
+
+    foo.a( function(f) { return f < 10 } )
+        // throws assertion if f is not less than 10
+
+-------------------------------------------------------------------------------
+
+        a: function( block, msg ) {
+            if ( arguments.length === 1 ) {
+                if (
+                        !(typeof block === 'function') &&
+                        !(block instanceof Function)
+                ) {
+                    msg = block;
+                    block = undefined;
+                }
+            } else if ( arguments.length >= 2 ) {
+                /*
+                 * If ...
+                 *  - block is not a function, and,
+                 *  - msg is a function,
+                 *  - then swap them!
+                 */
+                if (
+                        (
+                                !(typeof block === 'function') &&
+                                !(block instanceof Function)
+                        ) &&
+                        (
+                                (typeof   msg === 'function') && 
+                                (  msg instanceof Function)
+                        )
+                ) {
+                    msg = block;
+                    block = undefined;
+                }
+            }
+
+            var asserted = ( block !== undefined ) ?
+                    !! block.call( this, this ) :
+                    !! this                     ;
+
+            if ( ! asserted ) {
+                if ( msg ) {
+                    throw new Error( msg );
+                } else {
+                    throw new Error( "assertion error!" )
+                }
+            }
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## Block
+
+Function is in the form:
+
+```
+    f( obj, mark )
+
+If no mark has been placed,
+then mark will be undefined.
+
+The return value is ignored.
+
+@example 1,
+
+     foo.
+         b( function(obj) {
+             if ( obj ) {
+                 console.log( obj );
+             }
+         } ).
+         doWork();
+
+@example 2,
+
+     bar.m( 'work-object' );
+    
+     // some time later
+    
+     foo.
+         b( function(obj, mark) {
+             if ( mark === 'work-object' ) {
+                 console.log( obj );
+             }
+         } ).
+         doWork();
+
+@param cmd A block to pass this object into.
+@return This object.
+
+-------------------------------------------------------------------------------
+
+        b: function( cmd ) {
+            cmd.call( this, this, this.____mark____ );
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## Call
+
+Calls the command given, on this object.
+
+@param method The method required to be called.
+@return This object.
+
+-------------------------------------------------------------------------------
+
+        c: function( method ) {
+            var args = new Array( arguments.length-1 );
+            for ( var i = 1; i < arguments.length; i++ ) {
+                args[i-1] = arguments[i];
+            }
+
+            this[method].apply( this, args );
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## Debug
+
+Starts the debugger, if available.
+
+To select any object that is marked,
+just pass in true.
+
+@param mark Optional, debugger is only used if this has the same mark given.
+@return This.
+
+-------------------------------------------------------------------------------
+
+        d: function( mark ) {
+            if ( arguments.length === 0 ) {
+                debugger;
+            } else if ( mark === true || this.___mark___ === mark ) {
+                debugger;
+            }
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## Assert
+
+Asserts this is empty.
+
+@return This.
+
+-------------------------------------------------------------------------------
+
+        e: function() {
+            var isInvalid = false;
+
+            if ( this.length !== undefined ) {
+                if ( this.length > 0 ) {
+                    isInvalid = true;
+                }
+            } else {
+                for ( var k in this ) {
+                    if ( this.hasOwnProperty(k) ) {
+                        isInvalid = true;
+                        break;
+                    }
+                }
+            }
+
+            if ( isInvalid ) {
+                throw new Error("this object is not empty");
+            }
+                
+            return this;
+        },
+
+
+
+        f: function( field ) {
+            console.log( this[field] );
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## Keys
+
+Provides all of the keys for this object.
+This only includes keys which are on this
+objects property; it ignores prototypal
+properties.
+
+If a block is provided, then the keys will
+passed into that on each iteration instead
+of being outputted to the console.
+
+@param block Optional, a block for iterating across all keys.
+@return This object.
+
+-------------------------------------------------------------------------------
+
+        k: function( block ) {
+            for ( var k in this ) {
+                if ( this.hasOwnProperty(k) ) {
+                    if ( block ) {
+                        block.call( this, k );
+                    } else {
+                        console.log( k );
+                    }
+                }
+            }
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## Log
+
+Prints a message via console.log.
+If a msg is provided, it is printed,
+and otherwise this object is printed.
+
+@param Optional, a message to send to the console instead.
+@return This.
+
+-------------------------------------------------------------------------------
+
+        l: function( msg ) {
+            console.log( msg || this );
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## Mark
+
+Marking is here to help tracking objects, through a busy system.
+They are used to help distinguish objects later, without having to setup global
+variables.
+
+Other methods also take a mark, to allow filtering, so you can apply a method
+to lots of objects, and only those marked will actually carry out the action.
+
+### marking with a value
+
+The parameter given is the value to use
+when marking.
+
+This allows you to mark different objects,
+with different values, so they can be
+identified in different ways.
+
+### marking via block
+
+If called with a block,
+the object is passed into that block.
+
+If the block returns a non-falsy object,
+the object is then marked.
+
+If called with no block,
+it is marked for certain.
+
+This is to allow marking specific objects 
+in a large system, and then allow you to
+retrieve them again later.
+
+@param block An optional filter for marking objects, or the value to mark them with.
+@return This object.
+
+-------------------------------------------------------------------------------
+
+        m: function( block ) {
+            if ( block !== undefined ) {
+                if ( typeof block === 'function' || (block instanceof Function) ) {
+                    var mark = block.call( this, this );
+
+                    if ( mark ) {
+                        this.____mark____ = mark;
+                    } else {
+                        delete this.____mark____;
+                    }
+                } else {
+                    this.___mark___ = block || true;
+                }
+            } else {
+                this.____mark____ = true;
+            }
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## No Mark
+
+If this has a mark, then it is unmarked.
+The given block, like with mark,
+allows you to filter unmarking objects.
+
+@param block An optional filter to use for unmarking.
+@return This object.
+
+-------------------------------------------------------------------------------
+
+        n: function( block ) {
+            if ( this.____mark____ !== undefined ) {
+                if ( block !== undefined ) {
+                    if ( block.call(this, this, this.____mark____) ) {
+                        delete this.____mark____;
+                    }
+                } else {
+                    delete this.____mark____;
+                }
+            }
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## Print
+
+Prints this object to `console.log`.
+
+@param msg Optional, a message to print before the item.
+@return This.
+
+-------------------------------------------------------------------------------
+
+        p: function( msg ) {
+            if ( arguments.length === 0 ) {
+                console.log( this );
+            } else {
+                console.log( msg, this );
+            }
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## Stack trace.
+
+Prints a stack trace to the console.
+
+@return This object.
+
+-------------------------------------------------------------------------------
+
+        s: function() {
+            var err = new Error();
+
+            if ( err.stack ) {
+                console.log( err.stack );
+            }
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## Timestamp
+
+A timestamp is dumped to the console.
+
+@return This object.
+
+-------------------------------------------------------------------------------
+
+        t: function() {
+            console.log( Date.now() );
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## User Alert
+
+Shows an alert to the user.
+
+@param msg Optional, a message to display, defaults to this object.
+@return This.
+   
+-------------------------------------------------------------------------------
+
+        u: function( msg ) {
+            if ( arguments.length > 0 ) {
+                alert( msg );
+            } else {
+                alert( this );
+            }
+
+            return this;
+        },
+
+
+
+-------------------------------------------------------------------------------
+
+## Values
+
+This will iterate over all of the key => value pairs
+in this object. That is regardless of if this is
+an Array, Object, or something else.
+
+By default, they are printed.
+
+If a block is provided, they are passed to the block
+in turn.
+
+@example
+
+     foo.v( function(k, val) { ... } )
+
+@param block An optional block for iterating over the key-value pairs.
+@return This object.
+
+-------------------------------------------------------------------------------
+
+        v: function( block ) {
+            for ( var k in this ) {
+                if ( this.hasOwnProperty(k) ) {
+                    if ( block ) {
+                        block.call( this, k, this[k] );
+                    } else {
+                        console.log( k, this[k] );
+                    }
+                }
+            }
+
+            return this;
+        }
+
+
+
+    });
+
+
